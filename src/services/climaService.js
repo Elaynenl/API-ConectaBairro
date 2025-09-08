4f6770f13a781fc2347c7a73c3bb9b28b98a68fe
Igor Marcelo de Sousa Freire <124942186+IgorMsFreire@users.noreply.github.com>
2025-09-07 21:07:55 -0300

    Update climaService.js

---
+++ src/services/climaService.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const API_KEY = process.env.OPENWEATHER_API_KEY;
+
+export const buscarClima = async (cidade) => {
+  try {
+    const urlClima = https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API_KEY}&lang=pt_br&units=metric;
+    const resposta = await fetch(urlClima);
+
+    if (!resposta.ok) {
+      return { erro: "Não foi possível obter o clima para esta cidade" };
+    }
+
+    const dados = await resposta.json();
+    return {
+      cidade: dados.name,
+      temperatura: dados.main.temp,
+      descricao: dados.weather[0].description,
+      umidade: dados.main.humidity,
+      vento: dados.wind.speed
+    };
+  } catch (erro) {
+    return { erro: "Erro ao buscar clima", detalhe: erro.message };
+  }
+};
