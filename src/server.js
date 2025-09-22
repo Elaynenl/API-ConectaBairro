c1e5e6c0c1c7ed63f76acd8b0fa0873ef9e8fb66
Elayne Nascimento <156961330+Elaynenl@users.noreply.github.com>
2025-09-22 19:13:54 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,5 +3,6 @@ import app from "./app.js";
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
+
